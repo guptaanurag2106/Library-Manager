5a9013de1a895cf0df8bec1feefd87734f52f62b
ANURAG GUPTA <75490384+guptaanurag2106@users.noreply.github.com>
2021-04-25 23:16:06 +0530

    add books issued by users

--- users.js
+++ users.js
@@ -4,35 +4,40 @@ const users = [
         username: "user1",
         password: "pass1",
         type: "student",
-        loggedIn: false
+        loggedIn: false,
+        books: [1, 2, 3]
     },
     {
         id: 2,
         username: "user2",
         password: "pass2",
         type: "student",
-        loggedIn: false
+        loggedIn: false,
+        books: [1, 3]
     },
     {
         id: 3,
         username: "user3",
         password: "pass3",
         type: "student",
-        loggedIn: false
+        loggedIn: false,
+        books: [2, 4]
     },
     {
         id: 4,
         username: "user4",
         password: "pass4",
         type: "student",
-        loggedIn: false
+        loggedIn: false,
+        books: []
     },
     {
         id: 5,
         username: "user5",
         password: "pass5",
         type: "student",
-        loggedIn: false
+        loggedIn: false,
+        books: [1, 2, 3, 4]
     }
 ];
 
